98d123216de0171f71d26d88868f1d22ab9d4a06
Gloveman <bule3306@naver.com>
2016-07-30 01:44:16 +0900

    Button UI Changed
    너무많이바꾼듯....

--- raspbrary/raspbrary/Form1.Designer.cs
+++ raspbrary/raspbrary/Form1.Designer.cs
@@ -28,50 +28,60 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button2 = new System.Windows.Forms.Button();
+            this.components = new System.ComponentModel.Container();
             this.panel1 = new System.Windows.Forms.Button();
-            this.iBtn1 = new System.Windows.Forms.PictureBox();
-            ((System.ComponentModel.ISupportInitialize)(this.iBtn1)).BeginInit();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(278, 28);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(244, 422);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
             // 
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.Color.PaleGreen;
-            this.panel1.Location = new System.Drawing.Point(527, 28);
+            this.panel1.Location = new System.Drawing.Point(529, 28);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(244, 422);
             this.panel1.TabIndex = 3;
             this.panel1.UseVisualStyleBackColor = false;
             // 
-            // iBtn1
+            // imageList1
             // 
-            this.iBtn1.Image = global::raspbrary.Properties.Resources.search;
-            this.iBtn1.Location = new System.Drawing.Point(28, 28);
-            this.iBtn1.Name = "iBtn1";
-            this.iBtn1.Size = new System.Drawing.Size(244, 422);
-            this.iBtn1.TabIndex = 4;
-            this.iBtn1.TabStop = false;
-            this.iBtn1.Click += new System.EventHandler(this.iBtn1_Click);
+            this.imageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.imageList1.ImageSize = new System.Drawing.Size(16, 16);
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.White;
+            this.button2.BackgroundImage = global::raspbrary.Properties.Resources.add1;
+            this.button2.Image = global::raspbrary.Properties.Resources.add1;
+            this.button2.Location = new System.Drawing.Point(278, 28);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(244, 422);
+            this.button2.TabIndex = 1;
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // button1
+            // 
+            this.button1.BackgroundImage = global::raspbrary.Properties.Resources.search_modified;
+            this.button1.Image = global::raspbrary.Properties.Resources.search_modified;
+            this.button1.Location = new System.Drawing.Point(28, 28);
+            this.button1.Name = "button1";
+            this.button1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.button1.Size = new System.Drawing.Size(244, 422);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.BackColor = System.Drawing.SystemColors.ButtonShadow;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(800, 480);
-            this.Controls.Add(this.iBtn1);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.Font = new System.Drawing.Font("굴림", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
             this.ForeColor = System.Drawing.SystemColors.ControlText;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
@@ -80,15 +90,17 @@
             this.Padding = new System.Windows.Forms.Padding(26, 80, 26, 27);
             this.Text = "Rasprarian";
             this.TransparencyKey = System.Drawing.Color.Aqua;
-            ((System.ComponentModel.ISupportInitialize)(this.iBtn1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button panel1;
-        private System.Windows.Forms.PictureBox iBtn1;
+        private System.Windows.Forms.ImageList imageList1;
+
     }
 }
 
--- raspbrary/raspbrary/Form1.cs
+++ raspbrary/raspbrary/Form1.cs
@@ -16,7 +16,7 @@ namespace raspbrary
             InitializeComponent();
         }
 
-        private void iBtn1_Click(object sender, EventArgs e)
+        private void button1_Click(object sender, EventArgs e)
         {
 
         }
