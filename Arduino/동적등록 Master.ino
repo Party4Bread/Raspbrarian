f6242c2c1dfc6d8b1e4ab411ceb572f69d657773
DJdal <oonja0897@naver.com>
2016-10-30 17:06:45 +0900

    니 step 해봄
    컴파일 오류 x

---
+++ Arduino/동적등록 Master.ino
@@ -0,0 +1,33 @@
+#include<Wire.h>
+#define Slave 0
+
+void setup() {
+  Wire.begin();
+  Serial.begin(9600);
+
+}
+
+void loop() {
+   Wire.beginTransmission(Slave);
+   Wire.requestFrom(Slave, 1);
+   while(Wire.available()>1){
+   char a = Wire.read() ;
+   Serial.print(a);
+   }
+   Wire.endTransmission();
+   
+   Wire.beginTransmission(Slave);
+   while(Serial.available()>1){
+   char e = Serial.read();
+   byte c = e- 48;
+   Wire.write(c);
+   }
+   Wire.endTransmission();
+
+   Wire.beginTransmission(Slave);{
+    while(Wire.available()>1){
+      char b = Wire.read();
+      Serial.print(b);
+    }
+   }
+}
---
+++ Arduino/동적등록 Slave.ino
@@ -0,0 +1,34 @@
+#include<TinyWireS.h>
+#define Slave 0
+
+void requestEvent(void);
+void receiveEvent(uint8_t howmany);
+
+void setup() {
+    TinyWireS.begin(Slave);
+    TinyWireS.onRequest(requestEvent);
+    TinyWireS.onReceive(receiveEvent);
+}
+
+void loop() {
+
+}
+void receiveEvent(uint8_t howmany){
+  if( howmany <1){
+    return;
+  }
+  if( howmany >16){
+    return;
+  }
+  char data =  TinyWireS.receive();
+  howmany--;
+
+  TinyWireS.begin(data);
+  TinyWireS.send('OK');
+}
+
+void requestEvent(void){
+  TinyWireS.send('New');
+  
+}
+
